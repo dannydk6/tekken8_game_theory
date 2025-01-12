4584ae6e55430c494489d587d8a6cf1458361c72
dannydk6 <dfa238@nyu.edu>
2025-01-12 17:06:40 -0500

    modifications to moves based off bot

--- create_gbt.sh
+++ create_gbt.sh
@@ -1,1 +1,4 @@
-python json2gbt.py azu_12bplus8_reina.json -o azu_12bplus8_reina.gbt
+python json2gbt.py azu_12bplus8_reina.json -o artifacts/azu_12bplus8_reina.gbt
+
+
+python json2gbt.py azu_bt1_reina.json -o artifacts/azu_bt1_reina.gbt
