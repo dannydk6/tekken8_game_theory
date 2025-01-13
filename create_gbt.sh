cd8322fb6f9d22b99cdfe2691b082a15e6b2400b
dannydk6 <dfa238@nyu.edu>
2025-01-12 21:19:58 -0500

    recent tests into db3

--- create_gbt.sh
+++ create_gbt.sh
@@ -2,3 +2,7 @@ python json2gbt.py azu_12bplus8_reina.json -o artifacts/azu_12bplus8_reina.gbt
 
 
 python json2gbt.py azu_bt1_reina.json -o artifacts/azu_bt1_reina.gbt
+
+python json2gbt.py azu_db3_BTplus4_reina.json -o artifacts/azu_db3_BTplus4_reina.json
+
+python azucena_heat_engager_plus17.json -o artifacts/azucena_heat_engager_plus17.json
