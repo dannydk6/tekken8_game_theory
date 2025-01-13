8c8f0ceb363c13568c7485cd147106bb7e2f7b4f
dannydk6 <dfa238@nyu.edu>
2025-01-13 10:49:35 -0500

    notebook code

--- create_gbt.sh
+++ create_gbt.sh
@@ -1,8 +1,9 @@
-python json2gbt.py azu_12bplus8_reina.json -o artifacts/azu_12bplus8_reina.gbt
-
+python json2gbt.py azu_12bplus8_reina_v2.json -o artifacts/azu_12bplus8_reina_v2.gbt
 
 python json2gbt.py azu_bt1_reina.json -o artifacts/azu_bt1_reina.gbt
 
 python json2gbt.py azu_db3_BTplus4_reina.json -o artifacts/azu_db3_BTplus4_reina.json
 
-python azucena_heat_engager_plus17.json -o artifacts/azucena_heat_engager_plus17.json
+python json2gbt.py azucena_heat_engager_plus17.json -o artifacts/azucena_heat_engager_plus17.gbt
+
+python json2gbt.py azu_lib_plus8.json -o artifacts/azu_lib_plus8.gbt
